3625c6ba2d0a2ab0414577c05795db1240963a70
bhanu <bhanuprasad.bandaru54@gmail.com>
2026-07-15 23:04:18 +0530

    Add OptiCrop project files

---
+++ OPticrop/5.Project Development Phase/app.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+# Load model from the same folder as app.py
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/findyourcrop')
+def findyourcrop():
+    return render_template('findyourcrop.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+    output = prediction[0]
+
+    return render_template(
+        'findyourcrop.html',
+        prediction_text='Best crop for given conditions is {}'.format(output)
+    )
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
